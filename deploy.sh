388c72ea0cd03e4c9d37e181784d18ea2f799e90
BhargavaMadineni <160389106+BhargavaMadineni@users.noreply.github.com>
2024-03-19 14:49:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ Updating the script.
 hello i am updating this master branch.
 updating the line in dev branch.
 Hello i am updating this for stage branch. on 18/03
+adding this line toi check ssh test.
