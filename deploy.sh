081cdcff434508e77dd41528603f9926884cd754
BhargavaMadineni <160389106+BhargavaMadineni@users.noreply.github.com>
2024-03-19 15:00:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ hello i am updating this master branch.
 updating the line in dev branch.
 Hello i am updating this for stage branch. on 18/03
 adding this line toi check ssh test.
+updating for uat branch merge branches in remote repository.
